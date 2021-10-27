cd0418b3f004fc9b348efc4ce2cc90f6d8af40e7
jangsorry <piccolox@naver.com>
2021-10-28 05:29:33 +0900

    ...

--- DrinkWaterProject/DrinkWaterViewController.swift
+++ DrinkWaterProject/DrinkWaterViewController.swift
@@ -6,12 +6,6 @@
 //
 
 
-
-
-
-//작업해야할 것 UserDefaults.standard.set(number + 1, forKey: "number") 처럼 물먹는 양이 추가되도록 작업해야 할 것 같음
-//
-
 import UIKit
 
 class DrinkWaterViewController: UIViewController {
@@ -37,22 +31,28 @@ class DrinkWaterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.tintColor = .white
         
-        UIsetting()
+        //컬러는 포토샵 스포이드로 뽑은 후 -> rgb값 뽑아서 -> 컬러 적용
+        view.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
+        
+        NavUIsetting()
         labelUIDesign()
-        waterdata()
-     
+        setWaterDataTextField()
+       
+        onedayWaterLabel()
+        todayTotalmlLabel()
+        goalPercentLabel()
+        growMainImageView()
+        
+       
     }
     
-    
-    func UIsetting() {
+    func NavUIsetting() {
         self.title = "물마시기"
-        self.view.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
+        view.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
         resetButton.image = UIImage(systemName: "arrow.clockwise")
         profileButton.image = UIImage(systemName: "person.circle")
-    
-    
     }
     
     func labelUIDesign() {
@@ -68,27 +68,14 @@ class DrinkWaterViewController: UIViewController {
         totalLabel.textColor = .white
         totalLabel.font = .boldSystemFont(ofSize: 24)
         totalLabel.textAlignment = .left
-        
-        percentLabel.numberOfLines = 1
-        percentLabel.textColor = .white
-        percentLabel.font = .systemFont(ofSize: 16)
-        percentLabel.textAlignment = .left
-        
-        
+    
         
         //중간 UI
-        
-        waterTextField.placeholder = "0"
-        waterTextField.textAlignment = .right
-        waterTextField.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-        waterTextField.textColor = .white
-        
         unitLabel.text = "ml"
         unitLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         unitLabel.textColor = .white
-        
         infoLabel.font = .systemFont(ofSize: 14)
-        infoLabel.textColor = .white
+        infoLabel.textAlignment = .center
         
         //하단 UI 버튼
         drinkButton.setTitle("물마시기", for: .normal)
@@ -102,15 +89,111 @@ class DrinkWaterViewController: UIViewController {
     }
     
     
-    func waterdata() {
-        percentLabel.text = "목표의 50%"
-        totalLabel.text = "500ml"
-        infoLabel.text = "안녕하세요님의 하루 물 권장 섭취량은 100L 입니다."
+    func setWaterDataTextField() {
+        waterTextField.placeholder = "0"
+        waterTextField.keyboardType = .numberPad
+        waterTextField.borderStyle = .line
+        //색상값이 배경이랑 동일한데, 화면상에서는 다르게 나옴.. 여쭤봐야 할 듯
+        waterTextField.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
+        waterTextField.textColor = .white
+        waterTextField.textAlignment = .right
+        waterTextField.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        waterTextField.clearButtonMode = .always
         
+        view.endEditing(true)
         
     }
     
+    //좌측 상단 목표 퍼센트 label
+    func goalPercentLabel() {
+        let total = UserDefaults.standard.integer(forKey: "totalDrinkWater")
+        let todayRecommandWater = UserDefaults.standard.integer(forKey: "totalDrinkWater")
         
-   
-}
+        //0일때 여기서 오류 발생...
+        let percent = todayRecommandWater * 100 / total
+        UserDefaults.standard.set(percent, forKey: "percentage") //저장
+        let totalpercent = UserDefaults.standard.integer(forKey: "percentage")
+        percentLabel.text = "목표의\(totalpercent)%"
+        
+        if percent >= 100 {
+            percentLabel.textColor = .blue
+            percentLabel.font = .systemFont(ofSize: 18)
+            percentLabel.numberOfLines = 1
+            percentLabel.textAlignment = .left
+        } else {
+            percentLabel.textColor = .white
+            percentLabel.font = .systemFont(ofSize: 18)
+            percentLabel.numberOfLines = 1
+            percentLabel.textAlignment = .left
+        }
 
+        
+    }
+    //오늘 마신 양
+    func todayTotalmlLabel() {
+        totalLabel.text = "\(UserDefaults.standard.integer(forKey: "totalDrinkWater"))ml"
+    }
+        
+     //메인 이미지 구분
+    func growMainImageView() {
+            let totalpercent = UserDefaults.standard.integer(forKey: "percentage")
+            
+            switch totalpercent{
+            case 0...10: mainImageView.image = UIImage(named: "1-1")
+            case 11...20: mainImageView.image = UIImage(named: "1-2")
+            case 21...30: mainImageView.image = UIImage(named: "1-3")
+            case 31...40: mainImageView.image = UIImage(named: "1-4")
+            case 41...50: mainImageView.image = UIImage(named: "1-5")
+            case 51...60: mainImageView.image = UIImage(named: "1-6")
+            case 61...70: mainImageView.image = UIImage(named: "1-7")
+            case 71...80: mainImageView.image = UIImage(named: "1-8")
+            default: mainImageView.image = UIImage(named: "1-9")
+            }
+        }
+        
+        //하단 하루 물 권장 섭취량 Label
+        func onedayWaterLabel() {
+            let name : String = UserDefaults.standard.string(forKey: "name") ?? "안녕하세요"
+            var totalDrink = UserDefaults.standard.integer(forKey: "totalDrinkWater")
+            if totalDrink == 0 {
+                totalDrink = 2100
+            }
+            let totalDrinkLiter : Double =  Double(totalDrink) / 1000
+            
+            infoLabel.text = "\(name)님의 하루 물 권장 섭취량은 \(totalDrinkLiter)L입니다."
+         }
+    
+    //** 리셋. 값을 0으로 다시잡으면 실행함수들은 0으로 진행하는 것 같은데
+    //** 여기가 어떻게 해야할 지 몰랐던 부분 **
+    @IBAction func resetButtonClicked(_ sender: UIBarButtonItem) {
+        UserDefaults.standard.set(0, forKey: "totalDrinkWater")
+        
+        goalPercentLabel()
+        growMainImageView()
+        onedayWaterLabel()
+        todayTotalmlLabel()
+        
+     }
+    
+    
+    @IBAction func drinkButtonClicked(_ sender: UIButton) {
+        let nowDrink : Int = Int(waterTextField.text!) ?? 0
+        let todayDrink = UserDefaults.standard.integer(forKey: "totalDrinkWater")
+        
+        //여기서 동일키로 새로 저장해도 되는지 다시 찾아봐야할 듯함
+        UserDefaults.standard.set(todayDrink + nowDrink, forKey:"totalDrinkWater")
+        waterTextField.text = ""
+        goalPercentLabel()
+        growMainImageView()
+        onedayWaterLabel()
+        todayTotalmlLabel()
+        
+    }
+    
+    
+    @IBAction func tapClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    
+}
--- DrinkWaterProject/ProfileViewController.swift
+++ DrinkWaterProject/ProfileViewController.swift
@@ -20,24 +20,72 @@ class ProfileViewController: UIViewController {
     @IBOutlet var heightTextField: HoshiTextField!
     @IBOutlet var weighTextField: HoshiTextField!
         
-    
-    //텍스트필드는 userDefault로 저장한다음, 값을 가져오면 될 거 같고, 그 다음에 해당정보를 앞에 넘겨줘야할 듯..
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //view는 따로 위에 선언이 없어도 적용이 되는 것 같다..
+        view.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
         UISetting()
+        settinghoshiTextField()
 
        
     }
     
     func UISetting() {
         
-        self.view.backgroundColor = UIColor(red: 0/255, green: 151/255, blue: 112/255, alpha: 0.7)
-        
+     
         NavigationBarItem2.title = "저장"
     }
+    
+    func settinghoshiTextField() {
+        nicknameTextField.font = .systemFont(ofSize: 20)
+        nicknameTextField.placeholder = "닉네임을 설정해주세요"
+        nicknameTextField.placeholderColor = .white
+        nicknameTextField.textColor = .white
+        nicknameTextField.borderStyle = .none
+        nicknameTextField.borderActiveColor = .white
+        
+        
+        heightTextField.font = .systemFont(ofSize: 20)
+        heightTextField.placeholder = "키를 입력해주세요(cm)"
+        heightTextField.placeholderColor = .white
+        heightTextField.textColor = .white
+        heightTextField.borderStyle = .none
+        heightTextField.borderActiveColor = .white
+        heightTextField.keyboardType = .numberPad
+        
+        
+        weighTextField.font = .systemFont(ofSize: 20)
+        weighTextField.placeholder = "몸무게를 입력해주세요(kg)"
+        weighTextField.placeholderColor = .white
+        weighTextField.textColor = .white
+        weighTextField.borderStyle = .none
+        weighTextField.borderActiveColor = .white
+        weighTextField.keyboardType = .numberPad
+        
+        view.endEditing(true)
+        
+    }
 
+    
+    @IBAction func saveButtonClicked(_ sender: UIBarButtonItem) {
+        let nickname = nicknameTextField.text
+        UserDefaults.standard.set(nickname, forKey: "name")
+        
+        //더블타입으로 묶어줄때 옵셔널 경고떠서 그냥 강제해제하긴 했는데.. 이래도 되는 것인지 모르겠음..
+        let heightValue : Double = Double(heightTextField.text!) ?? 160
+        let weightValue : Double = Double(weighTextField.text!) ?? 50
+        
+        let total : Int = Int(heightValue + weightValue) * 10//
+        UserDefaults.standard.set(total, forKey: "totalDrinkWater")
+        
+    }
+    
+    @IBAction func tapClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    
 }
